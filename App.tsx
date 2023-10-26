46c795178a08bd41688c73fa9d1ab5bb9a6e0767
Janusz Majek <81168388+januszmajek@users.noreply.github.com>
2023-10-26 20:05:17 +0200

    lint

--- App.tsx
+++ App.tsx
@@ -8,19 +8,62 @@ import { NavigationContainer } from "@react-navigation/native";
 const appName = appConfig.expo.name;
 
 const theme = {
-  ...DefaultTheme,
-  colors: {
-    ...DefaultTheme.colors,
-  },
+	...DefaultTheme,
+	colors: {
+		primary: "rgb(0, 95, 175)",
+		onPrimary: "rgb(255, 255, 255)",
+		primaryContainer: "rgb(212, 227, 255)",
+		onPrimaryContainer: "rgb(0, 28, 58)",
+		secondary: "rgb(0, 99, 154)",
+		onSecondary: "rgb(255, 255, 255)",
+		secondaryContainer: "rgb(206, 229, 255)",
+		onSecondaryContainer: "rgb(0, 29, 50)",
+		tertiary: "rgb(0, 95, 175)",
+		onTertiary: "rgb(255, 255, 255)",
+		tertiaryContainer: "rgb(212, 227, 255)",
+
+
+    
+		onTertiaryContainer: "rgb(0, 28, 58)",
+		error: "rgb(186, 26, 26)",
+		onError: "rgb(255, 255, 255)",
+		errorContainer: "rgb(255, 218, 214)",
+		onErrorContainer: "rgb(65, 0, 2)",
+		background: "rgb(253, 252, 255)",
+		onBackground: "rgb(26, 28, 30)",
+		surface: "rgb(253, 252, 255)",
+		onSurface: "rgb(26, 28, 30)",
+		surfaceVariant: "rgb(224, 226, 236)",
+		onSurfaceVariant: "rgb(67, 71, 78)",
+		outline: "rgb(116, 119, 127)",
+		outlineVariant: "rgb(195, 198, 207)",
+		shadow: "rgb(0, 0, 0)",
+		scrim: "rgb(0, 0, 0)",
+		inverseSurface: "rgb(47, 48, 51)",
+		inverseOnSurface: "rgb(241, 240, 244)",
+		inversePrimary: "rgb(165, 200, 255)",
+		elevation: {
+			level0: "transparent",
+			level1: "rgb(240, 244, 251)",
+			level2: "rgb(233, 239, 249)",
+			level3: "rgb(225, 235, 246)",
+			level4: "rgb(223, 233, 245)",
+			level5: "rgb(218, 230, 244)",
+		},
+		surfaceDisabled: "rgba(26, 28, 30, 0.12)",
+		onSurfaceDisabled: "rgba(26, 28, 30, 0.38)",
+		backdrop: "rgba(45, 49, 56, 0.4)",
+	},
 };
 
 export default function App() {
-  return (
-    <PaperProvider theme={theme}>
-      <NavigationContainer>
-        <Navigation />
-      </NavigationContainer>
-    </PaperProvider>
-  );
+	return (
+		<PaperProvider theme={theme}>
+			<NavigationContainer>
+				<Navigation />
+			</NavigationContainer>
+		</PaperProvider>
+	);
 }
+
 AppRegistry.registerComponent(appName, () => App);
