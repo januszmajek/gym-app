2e0b2e3ecddd865ba90d9e7c937260a11e14a021
Janusz Majek <81168388+januszmajek@users.noreply.github.com>
2023-11-06 22:19:16 +0100

    dummy units on training screen

--- src/screens/TrainingScreen.tsx
+++ src/screens/TrainingScreen.tsx
@@ -1,18 +1,27 @@
 import { View, StyleSheet } from "react-native";
 import { Text } from "react-native-paper";
 import AppBar from "../components/AppBar";
-
+import useLengthStore from "../hooks/useLengthStore";
+import useWeightStore from "../hooks/useWeightStore";
+import useDistanceStore from "../hooks/useDistanceStore";
 export default function TrainingScreen() {
-	return (
-		<View>
-			<AppBar title="Trening" />
-			<Text variant="headlineMedium" style={styles.text}>
+  const { value: lengthUnit } = useLengthStore();
+  const { value: weightUnit } = useWeightStore();
+  const { value: distanceUnit } = useDistanceStore();
+
+  return (
+    <View>
+      <AppBar title="Trening" />
+      <Text variant="headlineMedium" style={styles.text}>
         Trening!
-			</Text>
-		</View>
-	);
+      </Text>
+      <Text>Biegasz w {distanceUnit}</Text>
+      <Text>Podnosisz {weightUnit}</Text>
+      <Text>Mierzysz bica w {lengthUnit}</Text>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-	text: { fontSize: 30, textAlign: "center" },
+  text: { fontSize: 30, textAlign: "center" },
 });
