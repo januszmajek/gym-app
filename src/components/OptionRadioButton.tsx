61db82ca295d3c7282032fd72861efdd13330098
Janusz Majek <81168388+januszmajek@users.noreply.github.com>
2023-11-07 22:06:25 +0100

    stash commit

--- src/components/OptionRadioButton.tsx
+++ src/components/OptionRadioButton.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { View, StyleSheet, Text, TouchableOpacity } from "react-native";
+import { View, StyleSheet, Text } from "react-native";
 import { Dialog, Portal, TouchableRipple } from "react-native-paper";
 import Radio from "./Radio";
 import useWeightStore from "../hooks/useWeightStore";
