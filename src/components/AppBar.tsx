31ee17cc3400643c988a716d6caeeb378ae87ed9
Janusz Majek <81168388+januszmajek@users.noreply.github.com>
2023-11-22 18:13:00 +0100

    add back from workout arrow

--- src/components/AppBar.tsx
+++ src/components/AppBar.tsx
@@ -1,14 +1,33 @@
 import * as React from "react";
-import { Appbar } from "react-native-paper";
+import { Appbar, useTheme } from "react-native-paper";
+import Icon from "react-native-vector-icons/MaterialCommunityIcons";
+import useWorkout from "../hooks/stores/useWorkout";
 
 interface AppBarProps {
   title: string;
 }
 
-const AppBar = ({ title }: AppBarProps) => (
-  <Appbar.Header>
-    <Appbar.Content title={title} />
-  </Appbar.Header>
-);
+const AppBar = ({ title }: AppBarProps) => {
+  const { colors } = useTheme();
+  const { activeWorkoutId, setActiveWorkoutId } = useWorkout();
+
+  const pressBackArrow = () => {
+    setActiveWorkoutId(0);
+  };
+
+  return (
+    <Appbar.Header>
+      <Appbar.Content title={title} />
+      {activeWorkoutId && (
+        <Icon
+          name="arrow-left"
+          size={32}
+          color={colors.secondary}
+          onPress={pressBackArrow}
+        />
+      )}
+    </Appbar.Header>
+  );
+};
 
 export default AppBar;
